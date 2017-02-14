8e6c05ca8808849b019e1ae7d0dc7b868103d01c
Build Tools <build.tools@example.com>
2017-02-14 14:02:32 +0000

    Fix lint errors

--- strain_gauge_calibration/scripts/calib_strain_gauges.py
+++ strain_gauge_calibration/scripts/calib_strain_gauges.py
@@ -33,15 +33,17 @@ class Calib_gauges():
             for key, s in enumerate(value.values):
 
                 fields[i, s.key] = s.value
-           
+
             # iterate over dictionary elements to search the wanted motor_id and the relative strain_gauge value
             for index, motor_id in fields.items():
 
-                if fields.get((index[0],'Motor ID')) == "%s" % self.motor_id:
+                if fields.get((index[0], 'Motor ID')) == "%s" % self.motor_id:
                     if self.strain_gauges_id == 0:
-                        self.measurement.append(fields.get((index[0], 'Strain Gauge Left')))  # store measurement in array
+                        # store measurement in array
+                        self.measurement.append(fields.get((index[0], 'Strain Gauge Left')))
                     else:
-                        self.measurement.append(fields.get((index[0], 'Strain Gauge Right')))  # store measurement in array
+                        # store measurement in array
+                        self.measurement.append(fields.get((index[0], 'Strain Gauge Right')))
 
     def run_test(self):
 
@@ -58,7 +60,8 @@ class Calib_gauges():
                 print("Apply %s g to strain_gauges %s" % (self.initial_weight, self.strain_gauges_id))
                 raw_input("Press enter when you are done...")
 
-                self.sub = rospy.Subscriber("/diagnostics_agg", DiagnosticArray, self.data_callback)  # Subscribe diagnostic topic to collect data
+                # Subscribe diagnostic topic to collect data
+                self.sub = rospy.Subscriber("/diagnostics_agg", DiagnosticArray, self.data_callback)
                 print("Measuring...")
 
                 rospy.sleep(5.0)  # give time to collect measurements
@@ -67,21 +70,22 @@ class Calib_gauges():
                 self.update_csv_file(self.measurement, self.strain_gauges_id)  # write measurements into csv file
                 self.measurement = []  # empty measurement array
 
-
     def update_csv_file(self, measurement, strain_gauges_id):
 
         if strain_gauges_id == 0:
             # create csv to write
-            csv_output = csv.writer(open("Motor_%s_strain_gauge_%s.csv" % (self.motor_id, strain_gauges_id), "wb"), lineterminator='\n')
+            csv_output = csv.writer(open("Motor_%s_strain_gauge_%s.csv" % (self.motor_id, strain_gauges_id), "wb"),
+                                    lineterminator='\n')
             self.headline_0.append(self.initial_weight)
             csv_output.writerow(self.headline_0)   # write headline row with weight values
             measurement = map(int, measurement)  # cast values to int
             self.temp_0.append(round(numpy.average(measurement)))  # compute values average
             csv_output.writerow(self.temp_0)  # write measurements
 
-        elif self.strain_gauges_id == 1:
+        elif strain_gauges_id == 1:
             # create csv to write
-            csv_output = csv.writer(open("Motor_%s_strain_gauge_%s.csv" % (self.motor_id, strain_gauges_id), "wb"), lineterminator='\n')
+            csv_output = csv.writer(open("Motor_%s_strain_gauge_%s.csv" % (self.motor_id, strain_gauges_id), "wb"),
+                                    lineterminator='\n')
             self.headline_1.append(self.initial_weight)
             csv_output.writerow(self.headline_1)  # write headline row with weight values
             measurement = map(int, measurement)
@@ -98,4 +102,3 @@ if __name__ == '__main__':
 # Output of the file Motor_11_strain_gauge_0.csv
 # 100,200,300,400,500
 # 0.0,-1.0,1.0,0.0,1.0
-

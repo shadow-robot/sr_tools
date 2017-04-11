01732c1f28d09f328decca059476bd9ae9bc3bc8
James Andrews <james@shadowrobot.com>
2017-04-11 10:09:02 +0000

    polygon visualisation (Example1) using PolyStamped

--- sr_grasp_stability/script/grasp_computation.py
+++ sr_grasp_stability/script/grasp_computation.py
@@ -4,6 +4,11 @@ import rospy
 from tf_computation import TfComputator
 from polygon_computation import Polygon
 
+from std_msgs.msg import Header
+from geometry_msgs.msg import Polygon, Point32, PolygonStamped
+
+pub = rospy.Publisher('grasp_quality_measure', Polygon, queue_size=10)
+
 rospy.init_node('sr_grasp_stability')
 
 TF_comp = TfComputator()  # Create an instance
@@ -12,12 +17,34 @@ Poly = Polygon()  # Create an instance
 trans = {}
 rot = {}
 
+
+point = Point32()
+
 while not rospy.is_shutdown():
 
     rate = rospy.Rate(10.0)
 
     trans, rot = TF_comp.get_finger_tips()
-    rospy.loginfo("The grasp 2d-polygon area is %s", Poly.measure_grasp_polygon_area(trans))
+    # rospy.loginfo("The grasp 2d-polygon area is %s", Poly.measure_grasp_polygon_area(trans))
     # print trans
+
+    poly_points = []
+
+    if 'rh_rfdistal' in trans:
+        for key, value in trans.items():
+            point.x, point.y, point.z = value
+            poly_points.append(point)
+
+            # print key, value
+
+    # print poly_points
+
+    polygon = Polygon(poly_points)
+
+    poly_stamped = PolygonStamped(polygon)
+
+    pub.publish(poly_stamped)
+
+    print poly_stamped
 
     rate.sleep()
--- sr_grasp_stability/script/tf_computation.py
+++ sr_grasp_stability/script/tf_computation.py
@@ -10,8 +10,10 @@ class TfComputator:
 
     def __init__(self):
 
+        # Dictionaries to store transformations
         self.trans = {}
         self. rot = {}
+
         self.tf_listener = tf.TransformListener()
 
     def get_finger_tips(self):
@@ -26,19 +28,19 @@ class TfComputator:
         # Get transforms from forearm to distal #
         try:
             (self.trans['rh_ffdistal'], self.rot['rh_ffdistal']) = self.tf_listener.lookupTransform(
-                '/rh_ffdistal', '/rh_forearm', rospy.Time(0))
+                '/rh_forearm', '/rh_ffdistal', rospy.Time(0))
 
             (self.trans['rh_lfdistal'], self.rot['rh_lfdistal']) = self.tf_listener.lookupTransform(
-                '/rh_lfdistal', '/rh_forearm', rospy.Time(0))
+                '/rh_forearm', '/rh_lfdistal', rospy.Time(0))
 
             (self.trans['rh_mfdistal'], self.rot['rh_mfdistal']) = self.tf_listener.lookupTransform(
-                '/rh_mfdistal', '/rh_forearm', rospy.Time(0))
+                '/rh_forearm', '/rh_mfdistal', rospy.Time(0))
 
             (self.trans['rh_rfdistal'], self.rot['rh_rfdistal']) = self.tf_listener.lookupTransform(
-                '/rh_rfdistal', '/rh_forearm', rospy.Time(0))
+                '/rh_forearm', '/rh_rfdistal', rospy.Time(0))
 
             (self.trans['rh_thdistal'], self.rot['rh_thdistal']) = self.tf_listener.lookupTransform(
-                '/rh_thdistal', '/rh_forearm', rospy.Time(0))
+                '/rh_forearm', '/rh_thdistal', rospy.Time(0))
 
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             rospy.loginfo("Failed to get tf for grasp measurement computation")

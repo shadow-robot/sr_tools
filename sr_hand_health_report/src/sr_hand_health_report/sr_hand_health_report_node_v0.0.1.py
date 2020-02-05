6df6b70a7a7584eb97d6964c0faa613c074c6226
giusebar <giuseppe@shadowrobot.com>
2020-02-05 18:41:32 +0000

    add hand_params, folder with serial and system info

--- sr_hand_health_report/src/sr_hand_health_report/sr_hand_health_report_node_v0.0.1.py
+++ sr_hand_health_report/src/sr_hand_health_report/sr_hand_health_report_node_v0.0.1.py
@@ -5,26 +5,41 @@ import rospy
 import rospkg
 import time
 import yaml
+import os
 from sr_hand_health_report_check import SrHealthReportCheck
 from monotonicity_check import MonotonicityCheck
 from position_sensor_noise_check import PositionSensorNoiseCheck
 from sr_hand_health_report.msg import CheckStatus
+from sr_system_info.system_info import SystemInfo
+from collections import OrderedDict 
 
 class HealthReportScriptNode(object):
     def __init__(self):
         self._health_report_checks_status_publisher = rospy.Publisher("/health_report_checks_status_publisher", CheckStatus, queue_size=1)
-        self._results = {"hand_info": {}, "checks": []}
+        self._results = {"hand_info": {}, "health_report_script_version": "v0.01", "checks": []}
+        self._system_info = SystemInfo()
+        self._system_info.collect()
+        self._results["system_info"] = self._system_info.values
         rospack = rospkg.RosPack()
-        self._results_path = "{}/sr_hand_health_reports/{}.yml".format(
+        self._hand_serial = self._get_hand_params()
+        self._hand_directory_path = "{}/sr_hand_health_reports/{}".format(
+                                    rospack.get_path('sr_hand_health_report'),
+                                    self._hand_serial)
+        if not os.path.exists(self._hand_directory_path):
+            os.makedirs(self._hand_directory_path)
+        self._results_path = "{}/sr_hand_health_reports/{}/{}.yml".format(
             rospack.get_path('sr_hand_health_report'),
-            time.strftime("%Y-%m-%d_%H-%M-%S"))
-        self._get_hand_params()
+            self._hand_serial,
+            time.strftime("health_report_results_%Y-%m-%d_%H-%M-%S"))
 
     def _get_hand_params(self):
         hand_params = rospy.get_param("hand")
         data = hand_params.get("mapping", "")
         hand_serial =  data.keys()[0]
-        self._results["hand_info"] = {"hand_serial": hand_serial}
+        hand_prefix = data.values()[0]
+        self._results["hand_info"]["hand_serial"] = hand_serial
+        self._results["hand_info"]["hand_prefix"] = hand_prefix
+        return hand_serial
 
     def _read_from_real_hand(self):
         """
@@ -46,17 +61,17 @@ class HealthReportScriptNode(object):
 
     def run_checks(self):
         """ run all the necessary checks """
-        check_name = "monotonicity_check"
-        self.publish_check_status(check_name)
-        monotonicity_check = MonotonicityCheck(args.hand_side)
-        monotonic_test_results = monotonicity_check.run_check()
-        self._results["checks"].append(monotonic_test_results)
+        # check_name = "monotonicity_check"
+        # self.publish_check_status(check_name)
+        # monotonicity_check = MonotonicityCheck(args.hand_side)
+        # monotonic_test_results = monotonicity_check.run_check()
+        # self._results["checks"].append(monotonic_test_results)
 
-        check_name = "position_sensor_noise_check"
-        self.publish_check_status(check_name)
-        position_sensor_noise_check = PositionSensorNoiseCheck(args.hand_side)
-        position_sensor_noise_results = position_sensor_noise_check.run_check()
-        self._results["checks"].append(position_sensor_noise_results)
+        # check_name = "position_sensor_noise_check"
+        # self.publish_check_status(check_name)
+        # position_sensor_noise_check = PositionSensorNoiseCheck(args.hand_side)
+        # position_sensor_noise_results = position_sensor_noise_check.run_check()
+        # self._results["checks"].append(position_sensor_noise_results)
         self.write_results_to_file()
 
     def write_results_to_file(self, filename=None):

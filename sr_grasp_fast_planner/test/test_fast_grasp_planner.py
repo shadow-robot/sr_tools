561f69670481251604317a84db9747d4f24baf4b
maxzieba <max@shadowrobot.com>
2021-05-04 11:26:43 +0000

    Lint check fixes

--- sr_grasp_fast_planner/test/test_fast_grasp_planner.py
+++ sr_grasp_fast_planner/test/test_fast_grasp_planner.py
@@ -15,6 +15,8 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import
+from builtins import round
+
 from math import log10, floor
 from unittest import TestCase
 

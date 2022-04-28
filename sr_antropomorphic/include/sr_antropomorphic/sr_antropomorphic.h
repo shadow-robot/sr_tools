0f5d2311e15acff4ec7417341b32fd4d4dc07bc2
maxzieba <max@shadowrobot.com>
2022-04-28 13:28:49 +0000

    first version

--- sr_antropomorphic/include/sr_antropomorphic/sr_antropomorphic.h
+++ sr_antropomorphic/include/sr_antropomorphic/sr_antropomorphic.h
@@ -1,23 +1,34 @@
 #include "ros/ros.h"
 #include <std_msgs/Float64MultiArray.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/robot_interaction/kinematic_options_map.h>
 #include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/robot_model/robot_model.h>
 #include <moveit/robot_state/robot_state.h>
-
+#include <geometry_msgs/Pose.h>
 #include <bio_ik/bio_ik.h>
+#include <yaml-cpp/yaml.h>
 #include <Eigen/Core>
 
 #include <math.h>
-#include <memory>
+
+struct FingerGroupParams{
+    std::string name;
+    tf2::Vector3 position_target;
+    tf2::Quaternion orientation_target;
+    double position_weight;
+    double orientation_weight;
+    double regularisation_weight;
+};
+
+
 
 class SrAntropomorphicIndex{
     public:
         SrAntropomorphicIndex();
         bool init();
         bool check_reachability(tf2::Vector3 position_target, tf2::Quaternion orientation_target);
-        bool check_finger_group_reachability(robot_model::JointModelGroup group, tf2::Vector3 position_target, tf2::Quaternion orientation_target);
-        bio_ik::BioIKKinematicsQueryOptions set_goal(std::string link_name, double pos_weight, double orient_weight);
+        bool check_all_reachability(std::vector<FingerGroupParams> finger_groups);
 
         robot_model_loader::RobotModelLoaderPtr robot_model_loader_;
         std::vector<robot_model::JointModelGroup*> robots_joint_groups_;
@@ -26,10 +37,17 @@ class SrAntropomorphicIndex{
         moveit::core::RobotStatePtr kinematic_state_;        
         std::vector<std::string> move_group_list_;
 
-        std::shared_ptr<const srdf::Model> srdf_model;
-        bio_ik::BioIKKinematicsQueryOptions kinematic_options_; // failing line
+        bio_ik::BioIKKinematicsQueryOptions kinematic_options_;
+        moveit::core::GroupStateValidityCallbackFn constraint_;  
 
     private:
         bool found_ik = false;
+        std::string side_prefix_ = "rh_";
+        double ik_timeout_ = 0.006;
+        double position_weight = 0.00;
+        double orientation_weight = 1;
+        double regularisation_weight = 1;
+        
+        std::vector<srdf::Model::EndEffector> model_eef_list_;
 
 };
--- sr_antropomorphic/src/sr_antropomorphic_node.cpp
+++ sr_antropomorphic/src/sr_antropomorphic_node.cpp
@@ -18,38 +18,27 @@ bool SrAntropomorphicIndex::init(){
     
     if (private_nh.getParam("move_group_list", move_group_list_)){
         ROS_INFO("Got move_group_list param");
+        for (auto group : kinematic_model_ -> getJointModelGroups()){
+            if (std::find(move_group_list_.begin(), move_group_list_.end(), group -> getName()) != move_group_list_.end()){
+                robots_joint_groups_.push_back(group);
+            }        
+        }
     }else{
         ROS_ERROR("Failed to get param move_group_list");
     }        
 
-    for (auto group : kinematic_model_ -> getJointModelGroups()){
-        if (std::find(move_group_list_.begin(), move_group_list_.end(), group -> getName()) != move_group_list_.end()){
-            robots_joint_groups_.push_back(group);
-        }        
-    }
-    
-    kinematic_state_.reset(new robot_state::RobotState(kinematic_model_));
-    srdf_model = kinematic_model_->getSRDF();
+    model_eef_list_ = kinematic_model_->getSRDF()->getEndEffectors();
+    kinematic_state_.reset(new robot_state::RobotState(kinematic_model_));    
+    kinematic_options_.replace = true;
+    kinematic_options_.return_approximate_solution = false;
 
     return true;
 };
 
 bool SrAntropomorphicIndex::check_reachability(tf2::Vector3 position_target, tf2::Quaternion orientation_target){
     
-    double position_weight = 0.99;
-    double orientation_weight = 0.01;
-    double regularisation_weight = 1.0;
-    double ik_timeout_  = 0.007;
-
-    //bio_ik::BioIKKinematicsQueryOptions kinematic_options_;
-    kinematic_options_.replace = true;
-    kinematic_options_.return_approximate_solution = false;
-
-    moveit::core::GroupStateValidityCallbackFn constraint_;    
-    std::string group_tip_link_name;
-    
+    std::string group_tip_link_name;    
     int nb_ik_solutions_found = 0;
-    auto model_eef_list_ = srdf_model->getEndEffectors();
 
     for (auto model : robots_joint_groups_){
         kinematic_options_.goals.clear();      
@@ -71,9 +60,9 @@ bool SrAntropomorphicIndex::check_reachability(tf2::Vector3 position_target, tf2
         orientation_goal->setOrientation(tf2::Quaternion(orientation_target));
         kinematic_options_.goals.emplace_back(orientation_goal);
 
-        auto* regularization_goal = new bio_ik::MinimalDisplacementGoal();
-        regularization_goal->setWeight(regularisation_weight);
-        kinematic_options_.goals.emplace_back(regularization_goal);       
+        //auto* regularization_goal = new bio_ik::MinimalDisplacementGoal();
+        //regularization_goal->setWeight(regularisation_weight);
+        //kinematic_options_.goals.emplace_back(regularization_goal);       
         
         found_ik = kinematic_state_->setFromIK(model, 
                                                EigenSTL::vector_Isometry3d(),
@@ -83,19 +72,20 @@ bool SrAntropomorphicIndex::check_reachability(tf2::Vector3 position_target, tf2
                                                kinematic_options_);
 
         const Eigen::Affine3d& ik_position = kinematic_state_->getGlobalLinkTransform(group_tip_link_name);
+        tf2::Vector3 ik_vec = tf2::Vector3(ik_position.translation().x(), ik_position.translation().y(), ik_position.translation().z());
+        double distance = ik_vec.distance(position_target);
 
-        auto x = position_target.getX()-ik_position.translation().x();
-        auto y = position_target.getY()-ik_position.translation().y();
-        auto z = position_target.getZ()-ik_position.translation().z();
-
-        std::cout << "Solution for " << group_tip_link_name << " found(" << ((int)found_ik) << ")" << std::endl;
         if (found_ik){
-            std::cout << "-Distance:" << sqrt(x*x+y*y+z*z) << std::endl;    
+            std::cout << "Solution for " << group_tip_link_name << " found" << std::endl;
+            std::cout << "-Distance:" << distance << std::endl;    
             nb_ik_solutions_found++;
             for (auto joint_model : model->getJointModels()){
                 std::cout << "--" << joint_model -> getName() << " " << 180*(*(kinematic_state_->getJointPositions(joint_model)))/3.1415 << std::endl;
             }
+        }else{
+            std::cout << "Solution for " << group_tip_link_name << " not found" << std::endl;
         }
+        break;
     }  
 
     if (nb_ik_solutions_found == robots_joint_groups_.size()){
@@ -104,19 +94,100 @@ bool SrAntropomorphicIndex::check_reachability(tf2::Vector3 position_target, tf2
     return false;
 }
 
+bool SrAntropomorphicIndex::check_all_reachability(std::vector<FingerGroupParams> finger_groups){
+    int nb_ik_solutions_found = 0;
+    for (FingerGroupParams group : finger_groups){
+        //std::cout << "Group name:" << group.name << std::endl;
+        //std::cout << "-" << "position target:[" << group.position_target.getX() << "," << group.position_target.getY() << "," << group.position_target.getZ() << "]" << std::endl;
+        //std::cout << "-" << "orientation target:[" << group.orientation_target.getX() << "," << group.orientation_target.getY() << "," << group.orientation_target.getZ() << "," << group.orientation_target.getW() << "]" << std::endl;
+
+        std::string group_tip_link_name;
+        kinematic_options_.goals.clear();      
+        for (int j=0; j < model_eef_list_.size(); j++){
+            if (model_eef_list_[j].parent_group_ == group.name){
+                group_tip_link_name = model_eef_list_[j].parent_link_;
+            }
+        }
+
+        if (group.position_weight > 0){
+            auto* position_goal = new bio_ik::PositionGoal();
+            position_goal->setLinkName(group_tip_link_name);
+            position_goal->setWeight(group.position_weight);
+            position_goal->setPosition(tf2::Vector3(group.position_target));
+            kinematic_options_.goals.emplace_back(position_goal);
+        }
+        if (group.orientation_weight > 0){
+            auto* orientation_goal = new bio_ik::OrientationGoal();
+            orientation_goal->setLinkName(group_tip_link_name);
+            orientation_goal->setWeight(group.orientation_weight);
+            orientation_goal->setOrientation(tf2::Quaternion(group.orientation_target));
+            kinematic_options_.goals.emplace_back(orientation_goal);
+        }
+        if (group.regularisation_weight > 0){
+            auto* regularization_goal = new bio_ik::MinimalDisplacementGoal();
+            regularization_goal->setWeight(group.regularisation_weight);
+            kinematic_options_.goals.emplace_back(regularization_goal);     
+        }  
+
+        bool found_ik = false;
+        found_ik = kinematic_state_->setFromIK(kinematic_model_->getJointModelGroup(group.name), 
+                                               EigenSTL::vector_Isometry3d(),
+                                               std::vector<std::string>(), 
+                                               ik_timeout_,
+                                               moveit::core::GroupStateValidityCallbackFn(),
+                                               kinematic_options_);
+
+
+        if(found_ik){
+            nb_ik_solutions_found++;
+            std::cout << "Found solution" << std::endl;
+        }else{
+            std::cout << "Not found!" << std::endl;
+        }
+
+    }
+    return nb_ik_solutions_found == finger_groups.size();
+}
+
 int main(int argc, char **argv){
     ros::init(argc, argv, "sr_antropomorphic_index_node");
-    SrAntropomorphicIndex ai = SrAntropomorphicIndex();
+    SrAntropomorphicIndex ai;
     ai.init();
 
+    std::vector<FingerGroupParams> finger_group_goals;
+
+    YAML::Node config = YAML::LoadFile("/home/user/projects/shadow_robot/base/src/sr_tools/sr_antropomorphic/config/test.yaml");
+
+    if (config["grasps"]) {
+        for (std::string finger_group : ai.move_group_list_){
+            auto position = config["grasps"]["large_diameter"][finger_group]["position"].as<std::vector<double>>();;
+            auto orientation = config["grasps"]["large_diameter"][finger_group]["orientation"].as<std::vector<double>>();;
+            tf2::Vector3 pos = tf2::Vector3(position[0], position[1], position[2]);
+            tf2::Quaternion orient = tf2::Quaternion(orientation[0], orientation[1], orientation[2], orientation[3]);
+            FingerGroupParams finger_group_param = {finger_group, pos, orient, 1, 0.01, 0.01};
+            finger_group_goals.push_back(finger_group_param);            
+        }
+        ai.check_all_reachability(finger_group_goals);
+    } 
+    
+    return 0;
+}
+
+
+/*
     // straight wrist (0, 0) and bent finger
     // - Translation: [0.032, -0.056, 0.402]
     // - Rotation: in Quaternion [0.666, -0.005, 0.004, 0.746]
 
+    tf2::Matrix3x3 matrix = tf2::Matrix3x3(0.98807, 0.0018493, -0.15398, -0.059824, 0.92599, -0.37277, 0.14189, 0.37753, 0.91506);           
+    tf2::Quaternion quat;
+    matrix.getRotation(quat);
+    std::cout<< quat.getX() << " " << quat.getY() << " " << quat.getZ() << " " << quat.getW();
     tf2::Vector3 pos_goal = tf2::Vector3(0.032, -0.056, 0.402);    
     tf2::Quaternion orient_goal = tf2::Quaternion(0.666, -0.005, 0.004, 0.746);
 
-    ai.check_reachability(pos_goal, orient_goal);
-    
-    return 0;
-}
+    //ai.check_reachability(pos_goal, orient_goal);
+    //pos_goal = tf2::Vector3(0.032, -0.056, 0.403);    
+    //orient_goal = tf2::Quaternion(0.666, -0.005, 0.004, 0.746);
+    //ai.check_reachability(pos_goal, orient_goal);
+*/

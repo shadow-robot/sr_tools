6c39ad849f7c6bb8b540c9804245dc368af52971
Ben Starmer-Smith <ben@shadowrobot.com>
2022-05-06 15:07:52 +0100

    Removed python2->python3 from future import

--- sr_world_generator/setup.py
+++ sr_world_generator/setup.py
@@ -1,5 +1,4 @@
 # ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
-from __future__ import absolute_import
 from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 

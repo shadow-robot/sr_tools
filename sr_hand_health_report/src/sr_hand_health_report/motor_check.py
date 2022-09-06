00505854ac3a7c800dd26e456b357e59d2dc1a89
maxzieba <max@shadowrobot.com>
2022-09-06 11:14:02 +0000

    Removing unused import

--- sr_hand_health_report/src/sr_hand_health_report/motor_check.py
+++ sr_hand_health_report/src/sr_hand_health_report/motor_check.py
@@ -13,7 +13,6 @@
 #
 # You should have received a copy of the GNU General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
-import sys
 import rospy
 from sr_hand_health_report_check import SrHealthReportCheck
 from diagnostic_msgs.msg import DiagnosticArray
